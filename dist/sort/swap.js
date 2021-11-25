6957873db0a5993202f9d0bd810616d64109b2d7
ctrl-cheeb-del <theredxer@gmail.com>
2021-11-25 09:25:02 +0000

    bubul sort

--- dist/sort/swap.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var swap = function (arr, i, j) {
-    var s = arr[i];
-    arr[i] = arr[j];
-    arr[j] = s;
-};
-exports.default = swap;
-//# sourceMappingURL=swap.js.map
--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -1,11 +1,16 @@
-import { SortAlgorithm } from "../common";
 import swap from "../swap";
 
-const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
+function bubbleSort<T>(array: T[]): T[] {
+  array = array.slice();
 
-    // Your code here
-
-    return items;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
+      }
+    }
+  }
+  return array;
 }
 
 export default bubbleSort;
