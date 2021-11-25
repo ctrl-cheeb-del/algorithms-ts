01a74b7e2b06c6d57ddb1530b169ef6da861da41
ctrl-cheeb-del <theredxer@gmail.com>
2021-11-25 09:33:12 +0000

    refactur

--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -1,8 +1,6 @@
 import swap from "../swap";
 
 function bubbleSort<T>(array: T[]): T[] {
-  array = array.slice();
-
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
       if (array[j] > array[j + 1]) {
