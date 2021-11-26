11dca6d9d7e265228bf2c87514d9ba50c998bf59
ctrl-cheeb-del <theredxer@gmail.com>
2021-11-26 16:16:53 +0000

    all sorts done :tada:

--- src/sort/mergeSort/mergeSort.ts
+++ src/sort/mergeSort/mergeSort.ts
@@ -1,10 +1,31 @@
-import { SortAlgorithm } from "../common";
+function mergeSort<T>(array: T[]): T[] {
+  if (!array.length) return array;
 
-const mergeSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
+  return mergeSortRecursive(array, 0, array.length - 1);
+}
 
-    // Your code here
+function mergeSortRecursive<T>(
+  array: T[],
+  lPointer: number,
+  rPointer: number
+): T[] {
+  if (lPointer == rPointer) return [array[lPointer]];
 
-    return items;
+  const middle = ~~((lPointer + rPointer) / 2);
+
+  const firstHalf = mergeSortRecursive(array, lPointer, middle);
+  const secondHalf = mergeSortRecursive(array, middle + 1, rPointer);
+
+  array = [];
+
+  while (firstHalf.length > 0 && secondHalf.length > 0) {
+    if (firstHalf[0] > secondHalf[0]) array.push(secondHalf.shift()!);
+    else array.push(firstHalf.shift()!);
+  }
+
+  array.push(...firstHalf, ...secondHalf);
+
+  return array;
 }
 
 export default mergeSort;
--- src/sort/quickSort/quickSort.ts
+++ src/sort/quickSort/quickSort.ts
@@ -1,11 +1,35 @@
-import { SortAlgorithm } from "../common";
 import swap from "../swap";
 
-const quickSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
+function quickSort<T>(array: T[]) {
+  if (array.length < 2) return array;
 
-    // Your code here
+  return quickSortRecursive(array, 0, array.length - 1);
+}
 
-    return items;
+function quickSortRecursive<T>(array: T[], lPointer: number, rPointer: number) {
+  if (lPointer < rPointer) {
+    const partitionIndex = partition(array, lPointer, rPointer);
+    quickSortRecursive(array, lPointer, partitionIndex - 1);
+    quickSortRecursive(array, partitionIndex + 1, rPointer);
+  }
+  return array;
+}
+
+function partition<T>(array: T[], lPointer: number, rPointer: number) {
+  const pivot = array[rPointer];
+
+  let i = lPointer - 1;
+
+  for (let j = lPointer; j < rPointer; j++) {
+    if (array[j] < pivot) {
+      i += 1;
+      swap(array, i, j);
+    }
+  }
+
+  swap(array, i + 1, rPointer);
+
+  return i + 1;
 }
 
 export default quickSort;
